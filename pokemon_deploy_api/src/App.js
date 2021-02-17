02dc6aaea5c014105262ad4c7a4869a54ff78808
Lucas <lucas.beraldin@gmail.com>
2021-02-17 15:55:40 +0000

    Pokemon image problem

--- pokemon_deploy_api/src/App.js
+++ pokemon_deploy_api/src/App.js
@@ -1,13 +1,33 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 
 function App() {
+
+  const [pokemon, setPokemon] = useState({});
+
+  useEffect(() => {
+
+    async function getPokemon() {
+      let response = await fetch("https://pokeapi.co/api/v2/pokemon/1", { headers: { accept: "application/json" } });
+
+
+      let data = await response.json();
+      console.log(data);
+      setPokemon(data);
+    }
+
+    getPokemon();
+  }, [])
+
   return (
-    <div className="App">
-
-
+    <div>
+      <h1>This is the Pokemon:</h1>
+      < img src={pokemon.sprites.front_default} />
     </div>
   );
 }
 
 export default App;
+
+
+
